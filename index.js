342ae811fd7c95766f3ece7cf17d80fc1d0e86ae
Danilopc <sr_danilo@hotmail.com>
2023-03-17 21:34:36 +0100

    server is runnig

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const app = express();
+const PORT = 8000;
+
+
+
+
+app.listen(PORT, () => console.log('server is running on port 8000'))
+
+
+
+
