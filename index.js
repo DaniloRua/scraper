d9ea1cf4d7665a0d81b6f888f959b0020d5c7f70
Danilopc <sr_danilo@hotmail.com>
2023-03-20 22:02:57 +0100

    scraper articles init

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-const express = require('express');
-const axios = require('axios');
+const PORT = 7000;
+const AXIOS = require('axios');
 const cheerio = require('cheerio');
+const express = require('express');
 const app = express();
-const PORT = 8000;
+const url = 'https://www.theguardian.com/international'
+
+
+// firtst example: Get articles from journal website
+AXIOS(url)
+    .then(response => {
+        const HTML = response.data;
+        const $ = cheerio.load(HTML);
+        const articles = []
+
+
+    //    $('.fc-item__title',).each(function(){
+    //     const title = $(this).text();
+    //     const url = $(this).find('a').attr('href');
+    //     articles.push({
+    //         title,
+    //         url
+    //     })
+    //     console.log(title[0])
+    //    })
+       
+
+    }).catch(err=>(console.log(err)))
 
 
 
 
-app.listen(PORT, () => console.log('server is running on port 8000'))
 
 
 
 
+
+
+
+
+
+
+
+
+
+;
+app.listen(PORT, () => console.log('server is running on port 7000'))
