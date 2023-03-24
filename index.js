047628cc988042a8d9ef4d0fbb4147dac4de1b11
Danilopc <sr_danilo@hotmail.com>
2023-03-24 18:08:38 +0100

    1 get articles from journal website

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ const AXIOS = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
 const app = express();
-const url = 'https://www.theguardian.com/international'
+const url = 'https://www.theguardian.com/world'
 
 
 // firtst example: Get articles from journal website
 AXIOS(url)
     .then(response => {
+        //load HTML content using axios
+        //Cheerio parses markup and provides an API for traversing/manipulating the resulting data structure.
         const HTML = response.data;
         const $ = cheerio.load(HTML);
-        const articles = []
-
-
-    //    $('.fc-item__title',).each(function(){
-    //     const title = $(this).text();
-    //     const url = $(this).find('a').attr('href');
-    //     articles.push({
-    //         title,
-    //         url
-    //     })
-    //     console.log(title[0])
-    //    })
-       
-
-    }).catch(err=>(console.log(err)))
-
-
+        let list = []
+        
+        //selecting the elements from journal webpage
+        //find a className to search
+        $('.fc-item__content').each(function () {
+            const title = $(this).find('span').text()
+            const link = $(this).find('a').attr('href');
+            list.push(
+                {title,
+                link
+            })
+        })
+        console.log(list)//will show articles + links
+    })
 
 
 
@@ -41,7 +40,4 @@ AXIOS(url)
 
 
 
-
-
-;
 app.listen(PORT, () => console.log('server is running on port 7000'))
